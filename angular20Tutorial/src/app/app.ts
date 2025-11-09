dc6820cb1da277990db0dd8e2b968393ce9000e1
Mosouri <mohommad.souri@gmail.com>
2025-11-09 15:04:38 +0300

    routing

--- angular20Tutorial/src/app/app.ts
+++ angular20Tutorial/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Admin } from './components/admin/admin';
 import { User } from './components/user/user';
 import { DataBinding } from './components/data-binding/data-binding';
 import { SignalEx } from './components/signal-ex/signal-ex';
+import { ControlFlow } from './components/control-flow/control-flow';
 
 @Component({
   selector: 'app-root',
-  imports: [SignalEx],
+  imports: [ControlFlow,RouterLink,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
